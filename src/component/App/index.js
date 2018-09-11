6ca88103631edc7ba162e85014c910084e6ccebf
HuQingyang <i@do.codes>
2018-09-12 01:29:10 +0800

    fix: eslint errors

---
+++ src/component/App/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { observer } from "mobx-react";
+
+import AppRoute from "../../route";
+
+function App() {
+  return <AppRoute />;
+}
+
+export default observer(App);
--- src/component/Posts/PostCard/index.js
+++ src/component/Posts/PostCard/index.js
@@ -1,37 +1,35 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {observer} from 'mobx-react';
-import {withStore} from "../../../store";
-import {sliceText} from '../../../utils';
-import {Link} from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { observer } from "mobx-react";
+import { withStore } from "../../../store";
+import { sliceText } from "../../../utils";
+import { Link } from "react-router-dom";
 
-import './index.scss';
+import "./index.scss";
 
-
-function PostCard({post}) {
-    const {
-        created, edited, title,
-        tags, content: intro, cover,
-        comments, id
-    } = post;
-    return (
-        <Link to={`/post/${id}`} className="post-card">
-            <div className="cover-container">
-                <img className="cover" src={cover} alt={title} />
-            </div>
-            <div className="post-card-info">
-                <h3 className="title">{title}</h3>
-                <hr />
-                <ul className="tags">{tags.map(t => <li key={t}>#{t}</li>)}</ul>
-                <p className="intro">{sliceText(intro, 50)}</p>
-            </div>
-        </Link>
-    );
+function PostCard({ post }) {
+  const { title, tags, content: intro, cover, id } = post;
+  return (
+    <Link to={`/post/${id}`} className="post-card">
+      <div className="cover-container">
+        <img className="cover" src={cover} alt={title} />
+      </div>
+      <div className="post-card-info">
+        <h3 className="title">{title}</h3>
+        <hr />
+        <ul className="tags">
+          {tags.map(t => (
+            <li key={t}>#{t}</li>
+          ))}
+        </ul>
+        <p className="intro">{sliceText(intro, 50)}</p>
+      </div>
+    </Link>
+  );
 }
 
 PostCard.propTypes = {
-    post: PropTypes.object
+  post: PropTypes.object
 };
-
 
 export default withStore(observer(PostCard));
